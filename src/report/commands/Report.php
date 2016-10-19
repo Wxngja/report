1794564c95a0e33521c57eb0d61676962bf78394
BoyIsDino <arsenalimran@hotmail.com>
2016-10-19 17:11:37 +0100

    Update Report.php

--- src/report/commands/Report.php
+++ src/report/commands/Report.php
@@ -25,21 +25,7 @@ class Report extends ReportTask {
 	         if (count($args) === 0) {
            $sender->sendMessage(SELF::prefix . TF::RED . $this->usageMessage;);
              }
-	  if (isset($args[0]) && strtolower($args[0]) === "report") {
-            if ($args[1] == "") {
-                $sender->sendMessage($this->prefix . TF::RED . "Failed to report a message!");
-            } else {
-                foreach ($this->getServer()->getOnlinePlayers() as $pl) {
-                    if ($pl->hasPermission("eoncore.report")) {
-                        $pl->sendMessage($this->prefix . TF::BLUE . "Report : " . TF::RED . $sender->getName() . " - " . TF::BLUE . $args[1] . " " .  TF::BLUE. $args[2] . " " .  TF::BLUE. $args[3] ." " .  TF::BLUE. $args[4] . " " .  TF::BLUE. $args[5] . " " .  TF::BLUE. $args[6] . " " .  TF::BLUE. $args[7] . " " .  TF::BLUE.$args[8] .  " " .  TF::BLUE.$args[9] .  " " .  TF::BLUE.$args[10]);
-                    } else {
-                        $sender->sendMessage($this->prefix . TF::RED . "Failed to report message as no operators are online!");
-                    }
-                }
-            }
-
-        }
-		}
+	      
 	    
 	    }
 }
