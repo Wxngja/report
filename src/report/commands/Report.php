89744dbdd42b4f76177363b58a9ce5d73a36ded2
BoyIsDino <arsenalimran@hotmail.com>
2016-10-19 17:47:01 +0100

    Update Report.php

--- src/report/commands/Report.php
+++ src/report/commands/Report.php
@@ -2,55 +2,62 @@
 namespace report\commands;
 use pocketmine\utils\Config;
 use pocketmine\utils\TextFormat as TF;
-use pocketmine\plugin\PluginBase;
-use pocketmine\event\Listener;
 use pocketmine\Player;
-use report\commands\Report;
+use report\Main;
+use pocketmine\command\CommandSender;
 use report\tasks\ReportTask;
-class Report extends ReportTask {
+class Report extends ReportTask
+{
 	private $plugin;
-	public function __construct(Main $plugin) {
-        $this->plugin = $plugin;
-        parent::__construct($plugin, "report", "Report a player!", "/report <player> <..> ", "r");
-    }
-	
-	public function getPlugin(){
-        return $this->plugin;
-    }
-   
+
+	public function __construct(Main $plugin)
+	{
+		$this->plugin = $plugin;
+		parent::__construct($plugin, "report", "Report a player!", "/report <player> <..> ", "r");
+	}
+
+	public function getPlugin()
+	{
+		return $this->plugin;
+	}
+
 	public $report;
-	    public function execute(CommandSender $sender, $commandLabel, array $args){
-             if($sender instanceof Player){
-		$this->report = new Config(($this->plugin->getDataFolder()."reports/".strtolower($sender->getName())."__report.yml", Config::
-	         if (count($args) === 0) {
-           $sender->sendMessage(SELF::prefix . TF::RED . $this->usageMessage;);
-             }
-	      if(isset($args[0]) && strtolower($args[0]) == ""){
-			$sender = $p;
-			$p->sendMessage(SELF::prefix . TF::GREEN . "Invalid Player!");
+
+	public function execute(CommandSender $sender, $commandLabel, array $args)
+	{
+		if ($sender instanceof Player) {
+			$this->report = new Config($this->plugin->getDataFolder() . "reports/" . strtolower($sender->getName()) . "__report.yml", Config::YAML);
+			if (count($args) === 0) {
+				$sender->sendMessage(Main::prefix . TF::RED . $this->usageMessage);
+			}
+			if (isset($args[0]) && strtolower($args[0]) == "") {
+				$p = $sender;
+				$p->sendMessage(Main::prefix . TF::GREEN . "Invalid Player!");
+			}
+			if (isset($args[1]) && strtolower($args[1]) == "") {
+				$p = $sender;
+				$p->sendMessage(Main::prefix . TF::GREEN . "Invalid message!");
+			}
+			if (isset($args[1]) && strtolower($args[1]) == "" && isset($args[0]) && strtolower($args[0]) == "") {
+				$p = $sender;
+				$p->sendMessage(Main::prefix . TF::GREEN . "Report failed...!");
+			}
+			$target = $this->plugin->getServer()->getPlayer($args[0]);
+			if ($target instanceof Player) {
+				if ($target == null) {
+					$p = $sender;
+					$p->sendMessage(Main::prefix . TF::RED . "ERROR: Target not found!");
+				}
+				$sender->sendMessage(Main::prefix . TF::RED . " Successfully reported " . TF::GREEN . $target->getName() . TF::RED . " for " . implode(" ", $args));
+				foreach ($this->plugin->getServer()->getOnlinePlayers() as $pl) {
+					if ($pl->hasPermission("report.bypass")) {
+						$pl->sendMessage(Main::prefix . TF::BLUE . "Report : " . TF::RED . $sender->getName() . " - " . TF::BLUE . $args);
+						$this->report->set("Report", $args);
+					} else {
+						$sender->sendMessage(Main::prefix . TF::RED . "Failed to report message as no operators are online!");
+					}
+				}
+			}
 		}
-	    if(isset($args[1]) && strtolower($args[1]) == ""){
-			$sender = $p;
-			$p->sendMessage(SELF::prefix . TF::GREEN . "Invalid message!");
-		}
-	   if(isset($args[1]) && strtolower($args[1]) == "" && isset($args[0]) && strtolower($args[0]) == "" ){
-			$sender = $p;
-			$p->sendMessage(SELF::prefix . TF::GREEN . "Report failed...!");
-		}
-			$target = $this->getServer()->getPlayer($args[0]);
-                  if($target instanceof Player) {
-			if($target == null){
-			$sender = $p;
-			$p->sendMessage(SELF::prefix . TF::RED . "ERROR: Target not found!");
-		}
-			     $sender->sendMessage(SELF::prefix . TF::RED . " Successfully reported " . TF::GREEN. $target->getName(); . TF::RED . " for " . implode(" ", $args));
-                   foreach ($this->getServer()->getOnlinePlayers() as $pl) {
-                    if ($pl->hasPermission("report.bypass")) {                         $pl->sendMessage(SELF::prefix . TF::BLUE . "Report : " . TF::RED . $sender->getName() . " - " . TF::BLUE . $args);
-                        $this->report->set("Report", $args);
-		    } else {
-                        $sender->sendMessage(SELF::prefix . TF::RED . "Failed to report message as no operators are online!");
-                    }
-                }
-		}
-	    }
+	}
 }
