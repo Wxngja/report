3082039d8c3070c8622bfc41541b34b97cbaa945
BoyIsDino <arsenalimran@hotmail.com>
2016-10-19 17:33:44 +0100

    Update Report.php

--- src/report/commands/Report.php
+++ src/report/commands/Report.php
@@ -25,7 +25,32 @@ class Report extends ReportTask {
 	         if (count($args) === 0) {
            $sender->sendMessage(SELF::prefix . TF::RED . $this->usageMessage;);
              }
-	      
-	    
+	      if(isset($args[0]) && strtolower($args[0]) == ""){
+			$sender = $p;
+			$p->sendMessage(SELF::prefix . TF::GREEN . "Invalid Player!");
+		}
+	    if(isset($args[1]) && strtolower($args[1]) == ""){
+			$sender = $p;
+			$p->sendMessage(SELF::prefix . TF::GREEN . "Invalid message!");
+		}
+	   if(isset($args[1]) && strtolower($args[1]) == "" && isset($args[0]) && strtolower($args[0]) == "" ){
+			$sender = $p;
+			$p->sendMessage(SELF::prefix . TF::GREEN . "Report failed...!");
+		}
+			$target = $this->getServer()->getPlayer($args[0]);
+                  if($target instanceof Player) {
+			if($target == null){
+			$sender = $p;
+			$p->sendMessage(SELF::prefix . TF::RED . "ERROR: Target not found!");
+		}
+			     $sender->sendMessage(SELF::prefix . TF::RED . " Successfully reported " . TF::GREEN. $target->getName(); . TF::RED . " for " . implode(" ", $args));
+                   foreach ($this->getServer()->getOnlinePlayers() as $pl) {
+                    if ($pl->hasPermission("report.bypass")) {                         $pl->sendMessage(SELF::prefix . TF::BLUE . "Report : " . TF::RED . $sender->getName() . " - " . TF::BLUE . $args);
+                        $this->report->set("Report", $args);
+		    } else {
+                        $sender->sendMessage(SELF::prefix . TF::RED . "Failed to report message as no operators are online!");
+                    }
+                }
+		}
 	    }
 }
