45415994d5cc88239f446bdba494ec9b10723cba
BoyIsDino <arsenalimran@hotmail.com>
2016-10-19 17:47:36 +0100

    Update Main.php

--- src/report/Main.php
+++ src/report/Main.php
@@ -1,22 +1,17 @@
 <?php
 namespace report;
 
-use pocketmine\utils\Config;
-use pocketmine\utils\TextFormat as TF;
 use pocketmine\plugin\PluginBase;
 use pocketmine\event\Listener;
-use pocketmine\Player;
+use pocketmine\utils\TextFormat as TF;
 use report\commands\Report;
 class Main extends PluginBase implements Listener{
- CONST $prefix = TF::BLACK . "[" . TF::RED . "Report" . TF::BLACK . "]" . TF::WHITE." ";
-
-    public function onEnable()
-    {
+ CONST prefix = TF::BLACK . "[" . TF::RED . "Report" . TF::BLACK . "]" . TF::WHITE." ";
+    public function onEnable(){
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
         @mkdir($this->getDataFolder());
         @mkdir($this->getDataFolder() . "reports/");
 		$this->getServer()->getCommandMap()->register("report", new Report($this));
-
     }
     
     }
